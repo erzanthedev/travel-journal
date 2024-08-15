67f03e9eade9ebcf58317817cd56628e196bfa79
Michael Erzan Andoh <erzanthedev@gmail.com>
2024-08-16 07:39:07 +1000

    feat: added and styled Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,10 @@
+import globe from "../assets/globe-icon.png";
+
 const Header = () => {
   return (
-    <header>
-      <h1>Hi</h1>
+    <header className="header">
+      <img className="header-logo" src={globe} alt="globe icon" />
+      <h4 className="header-title">my travel journal.</h4>
     </header>
   );
 };
