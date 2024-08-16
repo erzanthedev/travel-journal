9d7c41579b179938166f19913d2ae823f0994686
Michael Erzan Andoh <erzanthedev@gmail.com>
2024-08-16 10:40:47 +1000

    feat: added styling to allow scrolling

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -4,6 +4,13 @@ export default function MainContent() {
   return (
     <main>
       <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
     </main>
   );
 }
